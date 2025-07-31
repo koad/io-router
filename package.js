0cd812b8c781c9cec85a16da561ca52e962d0a18
koad <github@kingofalldata.com>
2025-07-31 00:07:46 -0400

    flash migration to meteor 3

--- package.js
+++ package.js
@@ -1,21 +1,18 @@
 Package.describe({
   name: 'koad:io-router',
-  summary: 'Routing specifically designed for Meteor',
-  version: '1.1.2'
+  summary: 'Routing specifically designed for koad:io',
+  version: '3.3.0',
 });
 
 Npm.depends({
-  'body-parser': '1.20.1'
+  'body-parser': '1.12.4'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.2');
+Package.onUse(function (api) {
+  api.versionsFrom(['3.0.2', '3.3']);
   // meteor dependencies
-  api.use('underscore');
+  api.use('koad:io-core');
   api.use('webapp', 'server');
-  api.use('deps', 'client');
-  api.use('ui');
-  api.use('templating');
 
   // for cloning
   api.use('ejson');
@@ -23,24 +20,51 @@ Package.onUse(function(api) {
   // for dynamic scoping with environment variables
   api.use('meteor');
 
-  // main namespace and utils
-  api.use('iron:core@1.0.11');
-  api.imply('iron:core');
+  // so our default_layout gets compiled
+  api.use('templating');
+  api.use('blaze');
 
-  // ui layout
-  api.use('iron:layout@1.0.12');
+  // some utils
+  api.use('underscore');
+  api.use('tracker'); // for Deps
 
-  // connect like middleware stack for client/server
-  api.use('iron:middleware-stack@1.1.0');
+  api.use('ui');
+  api.use('jquery');
+  api.use('reactive-var');
+  api.use('random');
+
+  api.use('appcache', {weak: true});
+
+  api.addFiles('lib/version_conflict_errors.js');
+  api.addFiles('lib/core.js');
+
+  // templating
+  api.addFiles('lib/dynamic_template.html');
+  api.addFiles('lib/dynamic_template.js');
+  api.addFiles('lib/blaze_overrides.js');
+
+  // add default layout for error pages
+  api.addFiles('lib/layout/templates.html');
+  api.addFiles('lib/layout/logic.js');
+
 
   // client and server side url utilities and compiling
-  api.use('iron:url@1.1.0');
+  api.addFiles('lib/compiler.js');
+  api.addFiles('lib/url.js');
 
-  // for reactive urls and pushState in the browser
-  api.use('iron:location@1.0.11');
+  // middleware
+  api.addFiles('lib/middleware_handler.js');
+  api.addFiles('lib/middleware_stack.js');
+
+  api.addFiles('lib/location/utils.js', 'client');
+  api.addFiles('lib/location/state.js', 'client');
+  api.addFiles('lib/location/location.js', 'client');
 
   // for RouteController which inherits from this
-  api.use('iron:controller@1.0.12');
+  api.addFiles('lib/wait_list.js', 'client');
+  api.addFiles('lib/controller.js');
+  api.addFiles('lib/controller_server.js', 'server');
+  api.addFiles('lib/controller_client.js', 'client');
 
   api.addFiles('lib/current_options.js');
   api.addFiles('lib/http_methods.js');
@@ -58,16 +82,19 @@ Package.onUse(function(api) {
   api.addFiles('lib/global_router.js');
   api.addFiles('lib/templates.html');
 
-
   // symbol exports
+  api.export('Handler', {testOnly: true});
+  api.export(['urlToHashStyle', 'urlFromHashStyle'], 'client', {testOnly: true});
+
+  api.export('Iron');
   api.export('Router');
   api.export('RouteController');
-
-  // api.imply('faisalman:ua-parser-js');
-  // api.use('multiply:iron-router-progress');
 });
 
+
 Package.onTest(function (api) {
+  api.versionsFrom('3.0.2');
+
   api.use('koad:io-router');
   api.use('tinytest');
   api.use('test-helpers');
@@ -76,4 +103,28 @@ Package.onTest(function (api) {
   api.addFiles('test/route_test.js');
   api.addFiles('test/router_test.js');
   api.addFiles('test/route_controller_test.js');
+
+  api.use('templating');
+  api.use('blaze');
+  api.use('deps');
+
+  api.addFiles('test/layout_test.html', 'client');
+  api.addFiles('test/layout_test.js', 'client');
+
+  api.addFiles('test/dynamic_template_test.html', 'client');
+  api.addFiles('test/dynamic_template_test.js', 'client');
+
+  api.addFiles('test/handler_test.js');
+  api.addFiles('test/middleware_stack_test.js');
+
+  api.addFiles('test/url_test.js', ['client', 'server']);
+
+  api.addFiles('test/location_test.js', 'client');
+
+  api.addFiles('test/controller_test.html', 'client');
+  api.addFiles('test/wait_list_test.js', 'client');
+  api.addFiles('test/controller_test.js', 'client');
+
 });
+
+
